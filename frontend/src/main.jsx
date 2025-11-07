90038da229d4e80018c0f323bdb4df348d570544
LimeshDev <dewanganlimesh6@gmail.com>
2025-11-07 21:22:08 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import AuthContext from './Context/authContext.jsx'
+import AuthContext from './Context/AuthContext.jsx'
 import UserContext from './Context/UserContext.jsx'
 import ShopContext from './Context/ShopContext.jsx'
 
