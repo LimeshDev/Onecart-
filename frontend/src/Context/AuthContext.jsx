c22d1d51a7781d4e09d94bd440aa0cfdb16516e7
LimeshDev <dewanganlimesh6@gmail.com>
2025-11-09 21:17:18 +0530

    Update AuthContext.jsx

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "https://mern-fullstack-shop-backend.onrender.com"
+    let serverUrl = "https://onecart-backendone-pgef.onrender.com/"
 
     let value = {
        serverUrl
